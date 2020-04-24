9bb01d673ec71f88cbdb415c271fc40fb362e557
Tomasz Wirkus <wirkus919@gmail.com>
2020-04-24 10:16:47 +0200

    Created forgotten information about user address in userPage

--- client/src/components/pages/auth/user/User.jsx
+++ client/src/components/pages/auth/user/User.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import UserNav from './UserNav';
+import UserNav from "./UserNav";
 
 export const User = ({ user }) => {
   return (
-    <section className='userPage'>
+    <section className="userPage">
       <UserNav />
-      <section className='userPage__about'>
-        <h2 className='userPage__about__header'>Dane użytownika</h2>
-        <ul className='userPage__about__list'>
-          <li className='userPage__about__list__item'>
+      <section className="userPage__about">
+        <h2 className="userPage__about__header">Dane użytownika</h2>
+        <ul className="userPage__about__list">
+          <li className="userPage__about__list__item">
             Imię i nazwisko: {user.name}
           </li>
-          <li className='userPage__about__list__item'>
+          <li className="userPage__about__list__item">
             Adres email: {user.email}
           </li>
-          <li className='userPage__about__list__item'>
+          <li className="userPage__about__list__item">
             Numer telefonu: {user.phone}
           </li>
-
-          <li className='userPage__about__list__item__address'>
-            <span className='userPage__about__list__item__address__text'>
-              Adres:{' '}
-            </span>
-            {user.address.town ? (
-              user.address.town
-            ) : (
-              <div className='userPage__about__list__item__address'>
-                <span className='userPage__about__list__item__address__text'>
-                  Brak adresu
-                </span>
-                <Link
-                  to='/settings'
-                  className='btn userPage__about__list__item__address__link'
-                >
-                  Dodaj adres
-                </Link>
-              </div>
-            )}{' '}
-          </li>
+          {user.address.town ? (
+            <div className="userPage__about__list__item__address">
+              <span className="userPage__about__list__item__address__text">
+                Kraj: {` ${user.address.country}`}
+              </span>
+              <span className="userPage__about__list__item__address__text">
+                Kod pocztowy: {` ${user.address.postCode}`}
+              </span>
+              <span className="userPage__about__list__item__address__text">
+                Miasto: {` ${user.address.postCode}`}
+              </span>
+              <span className="userPage__about__list__item__address__text">
+                Adres: {` ${user.address.street}`}
+              </span>
+            </div>
+          ) : (
+            <div className="userPage__about__list__item__address">
+              <span className="userPage__about__list__item__address__text">
+                Adres: Brak dodanego adresu
+              </span>
+              <Link
+                to="/settings"
+                className="btn userPage__about__list__item__address__link"
+              >
+                Dodaj adres
+              </Link>
+            </div>
+          )}{" "}
         </ul>
       </section>
     </section>
